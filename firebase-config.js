81a708e3a02afc1fe881a8ff7ff7409b5d194b94
casce1 <henryshum.cas@gmail.com>
2025-04-05 01:06:43 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+        const firebaseConfig = {
+            apiKey: "AIzaSyCYDawsdNAP5Epz_i-svaYfjQ310E7efKo",
+            authDomain: "casce1.firebaseapp.com",
+            projectId: "casce1",
+            storageBucket: "casce1.firebasestorage.app",
+            messagingSenderId: "460598433398",
+            appId: "1:460598433398:web:2b1f3d038e7576c4b57157",
+            measurementId: "G-LSQD078R21"
+        };
+
+        firebase.initializeApp(firebaseConfig);
